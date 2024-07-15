2563cccb2501d998d0261fb8550e67d42adc9c8d
rakati <ouhaddounouredine@gmail.com>
2024-07-15 12:24:01 +0100

    return to the earlier state fix bug in area and code style

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -11,13 +11,13 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        return (self.width * 4)
+    def permiter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
